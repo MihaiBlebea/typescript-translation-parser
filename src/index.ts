fb8f69a409fb3702aa18c31595601a5fb73d872d
Serban Blebea <slabestecuserban@gmail.com>
2019-11-16 17:14:53 +0000

    hot fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
 import TranslationParser from './TranslationParser'
 
-// export default TranslationParser
-
-let parser = new TranslationParser()
-
-parser.execute('./content.json').then((result)=> {
-    console.log(result)
-}).catch((error)=> {
-    console.log(error)
-})
+export default TranslationParser
